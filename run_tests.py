46eed7d97eb82f57c1121e853fb61d8dacd241dd
Cornflower Carp <dai.xufei@utexas.edu>
2023-09-06 17:49:50 +0000

    graph

--- run_tests.py
+++ run_tests.py
@@ -3,16 +3,19 @@ import os
 from subprocess import check_output
 import re
 from time import sleep
+import matplotlib
 
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 #
 #  Feel free (a.k.a. you have to) to modify this to instrument your code
 #
 
-THREADS = [0,1,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32]
-LOOPS = [100000]
-INPUTS = ["seq_64_test.txt","1k.txt","8k.txt","16k.txt"]
+THREADS = [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32]
+#THREADS = [0]
+LOOPS = [10]
+INPUTS = ["8k.txt"]
 
 csvs = []
 for inp in INPUTS:
@@ -21,7 +24,7 @@ for inp in INPUTS:
         for thr in THREADS:
             inputSize = inp.split(".")[0]
             outputName = "results/result_%s_t%d.txt" % (inputSize, thr)
-            cmd = "./bin/prefix_scan -o {} -n {} -i tests/{} -l {}".format(
+            cmd = "./bin/prefix_scan -o {} -n {} -i tests/{} -l {} -s".format(
                 outputName, thr, inp, loop)
             out = check_output(cmd, shell=True).decode("ascii")
             m = re.search("time: (.*)", out)
@@ -44,10 +47,12 @@ print(", ".join(header))
 for csv in csvs:
     print (", ".join(csv))
 
+
+plt.ticklabel_format(style='sci',scilimits=(0,0),axis='y')
 for csv in csvs:
-    plt.plot(THREADS,csv[1:],label=csv[0])
+    plt.plot(THREADS, list(map(int,csv[1:])), 'o-', label = csv[0])
 
 plt.xlabel("Number of threads")
-plt.ylabel("Run time")
+plt.ylabel("Run time in microseconds")
 plt.legend()
-plt.show()
+plt.savefig('10loop8k_s.png')
